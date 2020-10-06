8e7918559ec4e12ff17cbb46316d3ba8c5415f06
mgila <marianogila@gmail.com>
2020-10-06 14:27:08 -0700

    multer in process

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', 3000);
+
+// Middleware
+app.use(morgan('dev'));
+multer.diskStorage({
+    destination: path.join(__dirname. 'public/uploads'),
+    filename(req, file, cb) {
+        cb(null, );
+    }
+})
+app.use.(multer());
 
 // Start the server
 app.listen(app.get('port'), () => {
